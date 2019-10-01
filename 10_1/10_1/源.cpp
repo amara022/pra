6fac27c2101ab1e2c54834bcb65fa449da622edb
chen <1491534750@qq.com>
2019-10-01 22:56:56 +0800

    1

---
+++ 10_1/10_1/源.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+#include <stack>
+
+struct ListNode{
+	ListNode* next;
+	int val;
+};
+void PrintListReversingly_Iteratively(ListNode* pHead)
+{
+	std::stack<ListNode*> nodes;
+
+	ListNode* pNode = pHead;
+	while (pNode != nullptr)
+	{
+		nodes.push(pNode);
+		pNode = pNode->m_pNext;
+	}
+
+	while (!nodes.empty())
+	{
+		pNode = nodes.top();
+		printf("%d\t", pNode->m_nValue);
+		nodes.pop();
+	}
+}
+
+void PrintListReversingly_Recursively(ListNode* pHead)
+{
+	if (pHead != nullptr)
+	{
+		if (pHead->m_pNext != nullptr)
+		{
+			PrintListReversingly_Recursively(pHead->m_pNext);
+		}
+
+		printf("%d\t", pHead->m_nValue);
+	}
+}

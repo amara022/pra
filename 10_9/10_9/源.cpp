79eb33e522c38226d2f4cab3ff0db8facd388660
chen <1491534750@qq.com>
2019-10-09 21:12:52 +0800

    pow

---
+++ 10_9/10_9/源.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+int GetMyPower(int a, int n)
+{
+	int result = 1;
+	for (int i = 0; i < n; i++)
+	{
+		result *= n;
+	}
+	return result;
+}
+
+int GetMyPower2(int a, int n)
+{
+	if (n == 0)
+		return 1;
+	if (n == 1)
+		return a;
+	int result = GetMyPower2(a, n >> 1);
+	result *= result;
+	if (n &0x1 == 1)
+		//«Û”‡‘ÀÀ„
+		result *= a;
+	return result;
+}
+
+int main()
+{
+	cout << GetMyPower(2, 2) << endl;
+	cout << GetMyPower2(2, 5) << endl;
+	return 0;
+}
